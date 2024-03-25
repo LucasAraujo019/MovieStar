20fa331d884d1e453c0b39f46d890531150d2c35
unknown <lucas.oliveira019@hotmail.com>
2024-03-25 10:22:56 -0300

    Alterando senhas

--- user_process.php
+++ user_process.php
@@ -73,6 +73,31 @@
     // Atualizar senha do usuário   
     } else if($type === "changepassword"){
 
+        // Receber dados do post
+        $password = filter_input(INPUT_POST, "password");
+        $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+
+        // Resgata os dados do usuário
+        $userData = $userDao->verifyToken();
+        
+        $id = $userData->id;
+
+        if($password == $confirmpassword){
+            
+            // Criar um novo objeto de usuário
+            $user = new User();
+
+            $finalPassword = $user->generetePassword($password);
+
+            $user->password = $finalPassword;
+            $user->id = $id;
+
+            $userDao->changePassword($user);
+
+        } else {
+            $message->setMessage("As senhas não são iguais", "error", "back");
+        }
+
     } else {
         $message->setMessage("Informações inválidas!", "error", "index.php");
     }
