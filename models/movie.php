f36f4340e53db2c268c11351213e3a1ee2786d01
unknown <lucas.oliveira019@hotmail.com>
2024-04-16 09:56:28 -0300

    Incluir filmes

---
+++ models/movie.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Movie {
+   
+    public $id;
+    public $title;
+    public $description;
+    public $image;
+    public $trailer;
+    public $category;
+    public $length;
+    public $user_id;
+
+    public function imageGenerateName()
+    {
+        return bin2hex(random_bytes(60)). ".jpg";
+    }
+
+}
+    interface MovieDAOInterface {
+
+        public function buildMovie($data);
+        public function findAll ();
+        public function getLatesMovies();
+        public function getMovieByCategory($category);
+        public function getMovieByUserId($id);
+        public function findById($id);
+        public function findByTitle($title);
+        public function create(Movie $movie);
+        public function update(Movie $movie);
+        public function destroy(Movie $id);
+    }
+
+
+
+?>
